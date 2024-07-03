c967071b8f2d84813737ec81a448d5e7dfe48b70
Cedrick Mandocdoc <cedrickmandocdoc@gmail.com>
2024-07-03 17:38:47 +0800

    Task: Add models and constants

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+import { Country } from "./models";
+
+export const DEFAULT_COUNTRY: Country = {
+  code: "US",
+  name: "United States of America",
+};
+
+export const DEFAULT_LANGUAGE = "English - English";
+
+export const DEFAULT_CURRENCY = "USD - US Dollar";
